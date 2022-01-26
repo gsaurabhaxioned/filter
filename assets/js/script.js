b5b7f3264421271e7219b9fb87e8f3b6cc9d59d0
saurabh-gaonkar <saurabhg@axioned.com>
2022-01-26 13:28:01 +0530

    javascript code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-/* Author: 
-
-*/
-
-
-
-
-
+$(document).ready(function(){
+    $(".header-ham").click(function(){
+        $(".header-menus").slideToggle(1000);
+        $(this).toggleClass("cross");
+    });
+    $(".banner-ham").click(function(){
+        $(".banner-menus").slideToggle(1000);
+        $(this).toggleClass("cross");
+    });
+})
 
 
 
